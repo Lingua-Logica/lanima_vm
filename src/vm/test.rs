738d59b95d4dc09cb0c27eeb4b5fca14c5106a2c
APIBox <LinkerVB@outlook.com>
2025-10-20 13:57:11 +0800

    🔨 为 MOVRR 添加测试用例

--- src/vm/test.rs
+++ src/vm/test.rs
@@ -18,6 +18,22 @@ mod tests {
     }
 
     #[test]
+    fn test_movrr() {
+        let code = &[
+            make(OpCode::ADDI, &vec![0, 91]),
+            make(OpCode::MOVRR, &vec![1, 0]),
+            make(OpCode::PrintReg, &vec![0]),
+            make(OpCode::PrintReg, &vec![1]),
+        ];
+
+        let mut vm = Vm::new(code);
+
+        if let Err(err) = vm.run() {
+            println!("vm error: {err:#?}")
+        }
+    }
+
+    #[test]
     fn test_simple_program() {
         let code = &[];
 
